786c1c5a879c2bc892406c0c785b63cb8f43cac2
Kevin Galang <kgalang2@gmu.edu>
2018-02-28 15:47:17 -0500

    Add react router dom and route login and signup pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './Assets/default.css';
+import {
+	BrowserRouter as Router,
+	Route,
+} from 'react-router-dom';
 
 //components
 import LogIn from './components/pages/logIn';
+import SignUp from './components/pages/signUp';
 
 class App extends Component {
   render() {
     return (
       <div id="App">
-        <LogIn className="full login-page"/>
+        <Router>
+          <Route exact path="/" component={ LogIn } />
+          <Route exact path="/signup" component={ SignUp } />
+        </Router>
       </div>
     );
   }
