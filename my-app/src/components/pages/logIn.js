82d3d54f2192ffb02abdaf3825bca3dba31f823c
Kevin Galang <kgalang2@gmu.edu>
2018-03-07 09:36:52 -0500

    Connect log in page to store and add redux-thunk to dev dependencies

--- my-app/src/components/pages/logIn.js
+++ my-app/src/components/pages/logIn.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { login, logout } from "../../actions/login"
 
 import Slider from './logInSignUpComponent/Slider';
 import LogInForm from './logInSignUpComponent/LogInForm';
 
 class LogIn extends Component {
     render() {
-        return (
+        return this.props.loggedIn ? (
             <div className="full background-image">
                 <Slider className="slider-box" />
                 <LogInForm className="login-form full" />
             </div>
-        );
+        ) : (
+            <div>
+                <h1>You're Logged Out</h1>
+            </div>
+        )
     }
 }
 
-export default LogIn;
+export default connect(
+    state => ({
+        loggedIn: state.login.isLoggedIn
+    }),
+    () => ({})
+)(LogIn);
