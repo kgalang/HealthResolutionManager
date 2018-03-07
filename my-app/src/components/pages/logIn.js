66a4cf91c996a2f57fb80b079be08a362b1523d2
Kevin Galang <kgalang2@gmu.edu>
2018-03-07 11:33:14 -0500

    Make log in button dispatch action to store

--- my-app/src/components/pages/logIn.js
+++ my-app/src/components/pages/logIn.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { login, logout } from "../../actions/login"
+import { login, logout } from "../../actions/login";
+import { browserHistory } from "react-router";
 
 import Slider from './logInSignUpComponent/Slider';
 import LogInForm from './logInSignUpComponent/LogInForm';
+import * as loginActions from "../../actions/login";
 
 class LogIn extends Component {
+
     render() {
         return !this.props.loggedIn ? (
             <div className="full background-image">
                 <Slider className="slider-box" />
-                <LogInForm className="login-form full" />
+                <LogInForm className="login-form full" onClick={this.props.onClick}/>
             </div>
         ) : (
             <div>
@@ -24,5 +27,7 @@ export default connect(
     state => ({
         loggedIn: state.login.isLoggedIn
     }),
-    () => ({})
+    (dispatch) => ({
+        onClick: () => dispatch(loginActions.login())
+    })
 )(LogIn);
